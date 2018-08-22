7a5ccaf8f58c3ff854cb049f7f8cb56b7dc05988
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-08-21 23:07:07 -0400

    hello world react script. Using Components and Props. Pass values of an object as props into a component.

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// bring in React and Component from React
+
+import React, {Component} from 'react';
+
+// define our Hello component
+class Hello extends Component {
+  // what should the component render?
+  render () {
+    // make sure to return some UI
+    return (
+      <div>
+        <h1>Hello {this.props.name}!</h1>
+        <h3>You are {this.props.age} years old.</h3>
+        <p>You love: {this.props.animals[0]}</p>
+      </div>
+    )
+  }
+}
+
+export default Hello
