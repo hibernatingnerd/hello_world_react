eaad7df604abf96e7ca014494f391a82dd1ad20b
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-08-22 13:26:59 -0400

    add state for mood to hello_world. States can be updated inside components (unlike props)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,27 @@ import React, {Component} from 'react';
 
 // define our Hello component
 class Hello extends Component {
+  // what should happen when the component is first created
+  constructor (props) {
+    //make a call to parent
+    super()
+    //  define an initial state
+    this.state = {
+      moodPoints: 1 // initialize
+    }
+  }
+
+  increaseMood(e) {
+    if (this.state.moodPoints === 10){
+      this.setState({
+        moodPoints: 1
+      })
+    } else {
+      this.setState({
+        moodPoints: this.state.moodPoints +1
+      })
+    }
+  }
   // what should the component render?
   render () {
     // make sure to return some UI
@@ -12,6 +33,9 @@ class Hello extends Component {
         <h1>Hello {this.props.name}!</h1>
         <h3>You are {this.props.age} years old.</h3>
         <p>You love: {this.props.animals[0]}</p>
+        <p>On a scale of 1-10</p>
+        <p>You are this happy: {this.state.moodPoints}</p>
+        <button onClick={(e) => this.increaseMood(e)}>Cheer up!</button>
       </div>
     )
   }
